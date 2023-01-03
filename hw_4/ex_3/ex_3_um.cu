deb56bcbd2fc2ddeccec49db08a5b109dcba6e93
verach3n <kaixch3n@outlook.com>
2023-01-03 17:46:51 +0100

    ex3

---
+++ hw_4/ex_3/ex_3_um.cu
@@ -0,0 +1,117 @@
+#include <random>
+#include <stdio.h>
+#include <sys/time.h>
+#include <math.h>
+#include <cuda_profiler_api.h>
+
+#define DataType double
+#define bDim 16
+
+// Compute C = A * B
+__global__ void gemm(DataType *A, DataType *B, DataType *C, int numARows,
+                      int numAColumns, int numBRows, int numBColumns){
+  //@@ Insert code to implement matrix multiplication here
+  const int col = blockIdx.x * blockDim.x + threadIdx.x;
+  const int row = blockIdx.y * blockDim.y + threadIdx.y;
+  if ((col >= numBColumns) || (row >= numARows)) return;
+
+  DataType temp=0;
+  for(int i=0; i< numAColumns; i++){
+    temp +=  A[row*numAColumns+i] * B[numBColumns*i+col];
+  }
+  C[row*numBColumns+col] = temp;
+}
+
+double Second() {
+   struct timeval tp;
+   gettimeofday(&tp,NULL);
+   return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
+
+int main(int argc, char **argv) {
+  
+  DataType *A; // The A matrix
+  DataType *B; // The B matrix
+  DataType *C; // The output C matrix
+  DataType *resultRef; // The reference result
+
+  int numARows;    // number of rows in the matrix A
+  int numAColumns; // number of columns in the matrix A
+  int numBRows;    // number of rows in the matrix B
+  int numBColumns; // number of columns in the matrix B
+  int numCRows;
+  int numCColumns;
+
+  //@@ Insert code below to read in numARows, numAColumns, numBColumns from args
+  numARows = atoi(argv[1]);
+  numAColumns = atoi(argv[2]);
+  numBColumns = atoi(argv[3]);
+  numBRows = numAColumns;
+  numCRows = numARows;
+  numCColumns = numBColumns;
+  int m = numARows, k = numAColumns, n = numBColumns;
+  printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", m, k, k, n, m, n);
+  
+  //@@ Insert code below to allocate memory for input and output
+  size_t vec_sizeA = m*k*sizeof(DataType);
+  size_t vec_sizeB = k*n*sizeof(DataType);
+  size_t vec_sizeC = m*n*sizeof(DataType);
+  cudaProfilerStart();
+  cudaMallocManaged((void**)&A, vec_sizeA);
+  cudaMallocManaged((void**)&B, vec_sizeB);
+  cudaMallocManaged((void**)&C, vec_sizeC);
+  resultRef = (DataType*) malloc(vec_sizeC);
+  
+  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+  std::default_random_engine gen{};
+  std::uniform_real_distribution<DataType> dist{0, 1.0};
+  for(int i=0; i<m*k; i++) { 
+    A[i] = dist(gen); 
+  }
+  for(int i=0; i<k*n; i++) {
+    B[i] = dist(gen); 
+  }
+  
+  for(int p=0; p < m; p++){
+    for(int q=0; q < n; q++){
+      DataType temp=0;
+      for(int r=0; r < k; r++){
+        temp += A[k*p+r] * B[r*n+q];
+      }
+      resultRef[p*n+q] = temp;
+    }
+  }
+
+  //@@ Initialize the grid and block dimensions here
+  int Dgx, Dgy, Dbx, Dby;
+  Dgy = (m + bDim - 1) / bDim;
+  Dgx = (n + bDim - 1) / bDim;
+  Dbx = bDim;
+  Dby = bDim;
+
+  //@@ Launch the GPU Kernel here
+  gemm<<<dim3(Dgx, Dgy, 1), dim3(Dbx, Dby, 1)>>>(A, B, C, m, k, k, n);
+  cudaDeviceSynchronize();
+  cudaProfilerStop();
+
+  //@@ Insert code below to compare the output with the reference
+  DataType max_diff = 1e-9;
+  int flag = 0;
+  for (int i=0; i < m*n; i++) {
+    if (abs(C[i]-resultRef[i]) > max_diff) {
+      printf("Error!\n");
+      printf("Host Calculated Value: %f\n", resultRef[i]);
+      printf("Device Calculated Value: %f\n", C[i]);
+      flag = 1;
+    }
+  }
+  if(flag==0) printf("Equal!\n");
+
+  //@@ Free the GPU memory here
+  cudaFree(A);
+  cudaFree(B);
+  cudaFree(C);
+  free(resultRef);
+
+  return 0;
+}
